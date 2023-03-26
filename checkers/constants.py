5f0f60e0d4cd63858b1b7ef06a151b689eca1123
kryo1337 <kryo1337@gmail.com>
2023-03-26 09:47:24 +0100

    first commit

---
+++ checkers/constants.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+PADDING = 10
+OUTLINE = 2
+
+FPS = 60
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+
